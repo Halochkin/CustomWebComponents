961809412bbfc84d8b15e711799b1f4a1795c1ed
Maks Halochkin <33881747+Halochkin@users.noreply.github.com>
2018-06-26 15:40:08 +0300

    Update Swipe.js

--- Gestures/SwipeGestureMixin/src/Swipe.js
+++ Gestures/SwipeGestureMixin/src/Swipe.js
@@ -1,55 +1,27 @@
 const selectListener = Symbol("selectstartListener");
-const startListener = Symbol("pointerDownListener");
-const moveListener = Symbol("pointerMoveListener");
-const mouseStartListener = Symbol("mouseStartListener");
-const stopListener = Symbol("pointerUpListener");
-const start = Symbol("start");
 const move = Symbol("move");
-const end = Symbol("end");
-const swipe = Symbol("swipe");
+const touchStartListener = Symbol("downListener");
+const touchMoveListener = Symbol("touchMoveListener");
+const touchStopListener = Symbol("touchStopListener");
+const mouseStartListener = Symbol("mouseStartListener");
+const mouseMoveListener = Symbol("mouseMoveListener");
+const mouseStopListener = Symbol("mouseStopListener");
+const touchStart = Symbol("touchStart");
+const touchMove = Symbol("touchMove");
+const touchStop = Symbol("touchStop");
+const mouseStart = Symbol("mouseStart");
+const mouseMove = Symbol("mouseMove");
+const mouseStop = Symbol("mouseStop");
 const cachedEvents = Symbol("cachedEvents");
-const startCachedEvents = Symbol("startCachedEvents");
-const moveCachedEvents = Symbol("moveCachedEvents");
-const endCachedEvents = Symbol("endCachedEvents");
-const isTouchActive = Symbol("isTouchActive");
+const cachedDetail = Symbol("cachedDetail");
+const active = Symbol("active");
+const activeEventOrCallback = Symbol("activeEventOrCallback");
+const eventAndOrCallback = Symbol("callbackAndOrEvent");
+
 
 /**
- * SwipeGestureMixin adds a reactive lifecycle hook SwipeGestureCallback(...) to its subclasses.
- * This lifecycle hook is triggered every time a potentially assignable node for the element changes.
- * In order for mixin to support work with smartphones it was added both touch and mouse events.
- * SwipeGestureCallback(...) triggers manually every time the "swipe" event has been activated
- ** The "swipe" event only occurs if the pointermove events have:
- *  - moved a minimum 50px
- *  - in one direction
- *  - during the last 200ms.
- *
- * The minimum distance and duration can be changed using these properties on the element
- *    .flingSettings.minDistance = 50;
- *    .flingSettings.minDuration = 200;
- *    .flingSettings. maxTouches: 1.
- *
- * SwipeGestureCallback(startDetails, moveDetails, endDetails) contain:
- * 1 - startDetails:
- *    .startX,
- *    .startY,
- *    .event[e].
- *
- * 2 - moveDetails:
- *    .stopX,
- *    .stopY,
- *    .distX,
- *    .distY,
- *    .diagonalPx,
- *    .durationMs,
- *    .speedPxMs,
- *    .xSpeedPxMs,
- *    .ySpeedPxMs,
- *    .angle
- *
- * 3 - endDetails
- *    .stopX,
- *    .stopY:
- *    .event:
+ * SwipeGestureMixin adds swipe events and/or callbacks to an element.
+
  * The angle starts at 12 o'clock and counts clockwise from 0 to 360 degrees.
  *  up/north:     0
  *  right/east:  90
@@ -57,150 +29,137 @@ const isTouchActive = Symbol("isTouchActive");
  *  left/west:  270
  *
  * @returns {SwipeGestureMixin} class that extends HTMLElement
+ * @param event
  * @param x
  * @param y
+ * @param startDetail
  */
 
-function flingAngle(x = 0, y = 0) {
-  return ((Math.atan2(y, -x) * 180 / Math.PI) + 270) % 360;
+
+function makeDetail(event, x, y, startDetail) {
+  const distX = x - startDetail.x;
+  const distY = y - startDetail.y;
+  const distDiag = Math.sqrt(distX * distX + distY * distY);
+  const durationMs = event.timeStamp - startDetail.event.timeStamp;
+  return {event, x, y, distX, distY, distDiag, durationMs};
 }
 
-function findLastEventOlderThan(events, timeTest) {
-  for (let i = events.length - 1; i >= 0; i--) {
-    if (events[i].timeStamp < timeTest)
-      return events[i];
-  }
-  return null;
-}
-
-/**
- * !!! Dependency: pointerevents !!!
- *
- * Mixin that translates 0.0.1 sequence of one pointerdown and several pointermove events into one or more swipe events.
- *
- * The "swipe" event only occurs if the pointermove events have:
- *  - moved 0.0.1 minimum 50px
- *  - in one direction
- *  - during the last 200ms.
- *
- * The minimum distance and duration can be changed using these properties on the element
- *   .flingSettings.minDistance = 50;
- *   .flingSettings.minDuration = 200;
- *
- * swipe.detail
- *                .x
- *                .y
- *                .distX
- *                .distY
- *                .diagonalPx
- *                .durationMs
- *                .speedPxMs
- *                .pointerevent: 0.0.5
- *                .xSpeedPxMs
- *                .ySpeedPxMs
- *                .angle
- *
- * The angle starts at 12 o'clock and counts clockwise from 0 to 360 degrees.
- *  up/north:     0
- *  right/east:  90
- *  down/south: 180
- *  left/west:  270
- *
- * !!! Dependency: pointerevents !!!
- * !!! for Safari and older browsers use PEP: https://github.com/jquery/PEP !!!
- *
- * @param Base
- * @returns {SwipeGesture}
- */
 export const SwipeGestureMixin = function (Base) {
   return class extends Base {
     constructor() {
       super();
       this[selectListener] = e => e.preventDefault() && false;
-      this[startListener] = (e) => this[start](e);
-      this[moveListener] = (e) => this[move](e);
-      this[stopListener] = (e) => this[end](e);
-      this[startCachedEvents] = undefined;
-      this[moveCachedEvents] = undefined;
-      this[endCachedEvents] = undefined;
-      this.swipeSettings = {minDistance: 50, minDuration: 200,  maxTouches: 1};
+      this[touchStartListener] = e => this[touchStart](e);
+      this[touchMoveListener] = e => this[touchMove](e);
+      this[touchStopListener] = e => this[touchStop](e);
+      this[mouseStartListener] = e => this[mouseStart](e);
+      this[mouseMoveListener] = e => this[mouseMove](e);
+      this[mouseStopListener] = e => this[mouseStop](e);
+      this[cachedEvents] = undefined;
+      this[active] = 0;       //0 = inactive, 1 = mouse, 2 = touch
+      this[activeEventOrCallback] = 0; //caches the result of static get swipeEventOrCallback() for each event sequence
+    }
+
+    static get swipeSettings() {
+      return {minDistance: 100, minDuration: 200, maxTouches: 1};
+    }
+
+    /**
+     * By default it is only event
+     * @returns {number} 0 = event+callback, 1 = only event, -1 = only callback
+     */
+    static get swipeEventOrCallback() {
+      return 0;
     }
 
     connectedCallback() {
       if (super.connectedCallback) super.connectedCallback();
       this.addEventListener("selectstart", this[selectListener]);
-      this.addEventListener("touchstart", this[startListener]);
-      this.addEventListener("mousedown", this[startListener]);
+      this.addEventListener("touchstart", this[touchStartListener]);
+      this.addEventListener("mousedown", this[mouseStartListener]);
     }
 
     disconnectedCallback() {
       if (super.disconnectedCallback) super.disconnectedCallback();
       this.removeEventListener("selectstart", this[selectListener]);
-      this.removeEventListener("touchstart", this[startListener]);
-      this.removeEventListener("mousedown", this[startListener]);
+      this.removeEventListener("touchstart", this[touchStartListener]);
+      this.removeEventListener("mousedown", this[touchStartListener]);
     }
 
-    [start](e) {
-      this[isTouchActive] = (e.type === "touchstart");
-      if (this[isTouchActive]) {
-        this.addEventListener("touchmove", this[moveListener]);
-        this.addEventListener("touchend", this[stopListener]);
-        this.addEventListener("touchcancel", this[stopListener]);
+    [touchStart](e) {
+      if (this[active] === 1)
+        return;
+      if (this[active] === 2)   //this will be a second touch
+        return this[touchStopListener]();
+      this[active] = 2;
+      this[activeEventOrCallback] = this.constructor.swipeEventOrCallback;
+      window.addEventListener("touchmove", this[touchMoveListener]);
+      window.addEventListener("touchend", this[touchStopListener]);
+      window.addEventListener("touchcancel", this[touchStopListener]);
+      const detail = {event: e, x: e.targetTouches[0].pageX, y: e.targetTouches[0].pageY};
+      this[cachedEvents] = [detail];
+      this[eventAndOrCallback]("swipestart", detail);
+    }
+
+    [mouseStart](e) {
+      if (this[active] === 2)
+        return;
+      this[active] = 1;
+      this[activeEventOrCallback] = this.constructor.swipeEventOrCallback;
+      window.addEventListener("mousemove", this[mouseMoveListener]);
+      window.addEventListener("mouseup", this[mouseStopListener]);
+      const detail = {event: e, x: e.x, y: e.y};
+      this[cachedEvents] = [detail];
+      this[eventAndOrCallback]("swipestart", detail);
+    }
+
+    [mouseMove](e) {
+      this[move](e, e.x, e.y);
+    }
+
+    [touchMove](e) {
+      this[move](e, e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+    }
+
+    [move](event, x, y) {
+      const prevDetail = this[cachedEvents][this[cachedEvents].length - 1];
+      const detail = makeDetail(event, x, y, prevDetail);
+      this[eventAndOrCallback]("swipe", detail);
+    }
+
+    [mouseStop](e) {
+      const prevDetail = this[cachedEvents][this[cachedEvents].length - 1];
+      const detail = makeDetail(event, e.x, e.y, prevDetail);
+      if (Math.abs(detail.distX) < this.constructor.swipeSettings.minDistance) {
+        return;
       }
-      if (!this[isTouchActive])
-        this.addEventListener("mousemove", this[moveListener]);
-      this.addEventListener("mouseup", this[stopListener]);
-      this.addEventListener("mouseout", this[stopListener]);
-      this[startCachedEvents] = {
-        x: this[isTouchActive] ? e.targetTouches[0].pageX : e.x,
-        y: this[isTouchActive] ? e.targetTouches[0].pageY : e.y,
-        events: [e]
-      };
+      this[eventAndOrCallback]("swipeend", detail);
+      window.removeEventListener("mousemove", this[mouseMoveListener]);
+      window.removeEventListener("mouseup", this[mouseStopListener]);
+      this[cachedEvents] = undefined;
+      this[active] = 0;
+      this[activeEventOrCallback] = undefined;
     }
 
-    [move](e) {
-      if (this[isTouchActive] && e.targetTouches.length > this.flingSettings.maxTouches) //Added restriction on the number of fingers  //todo Should I add it or not?
-        return;
-      const swipeStart = findLastEventOlderThan(this[startCachedEvents].events, e.timeStamp - this.swipeSettings.minDuration);
-      if (!swipeStart)
-        return;
-      const lastX = this[isTouchActive] ? e.targetTouches[0].pageX : e.x;
-      const lastY = this[isTouchActive] ? e.targetTouches[0].pageY : e.y;
-      const distX = this[isTouchActive] ? lastX - swipeStart.targetTouches[0].pageX : lastX - swipeStart.x;
-      const distY = this[isTouchActive] ? lastY - swipeStart.targetTouches[0].pageY : lastY - swipeStart.y;
-      const diagonalPx = Math.sqrt(distX * distX + distY * distY);
-      if (diagonalPx < this.swipeSettings.minDistance)
-        return;
-      const durationMs = e.timeStamp - swipeStart.timeStamp;
-      this[moveCachedEvents] = {
-        lastX,
-        lastY,
-        distX,
-        distY,
-        diagonalPx,
-        durationMs,
-        speedPxMs: diagonalPx / durationMs,
-        xSpeedPxMs: (lastX - swipeStart.x) / durationMs,
-        ySpeedPxMs: distY / durationMs,
-        angle: flingAngle(lastX - swipeStart.x, distY),
-        event: e
-      };
+    [touchStop](e) {
+      const prevDetail = this[cachedEvents][this[cachedEvents].length - 1];
+      const detail = makeDetail(event, e.changedTouches[0].clientX, e.changedTouches[0].clientY, prevDetail);
+      this[eventAndOrCallback]("swipeend", detail);
+      window.removeEventListener("touchmove", this[touchMoveListener]);
+      window.removeEventListener("touchend", this[touchStopListener]);
+      window.removeEventListener("touchcancel", this[touchStopListener]);
+      this[cachedEvents] = undefined;
+      this[active] = 0;
+      this[activeEventOrCallback] = undefined;
     }
 
-    [end](e) {
-      this.removeEventListener("touchmove", this[moveListener]);
-      this.removeEventListener("touchend", this[stopListener]);
-      this.removeEventListener("touchcancel", this[stopListener]);
-      this.removeEventListener("mousemove", this[moveListener]);
-      this.removeEventListener("mouseup", this[stopListener]);
-      this.removeEventListener("mouseout", this[stopListener]);
-      if (!this[moveCachedEvents])
-        return;
-      this.swipeGestureCallback(this[startCachedEvents], this[moveCachedEvents]);
-      this[startCachedEvents] = undefined;
-      this[moveCachedEvents] = undefined;
-      this[endCachedEvents] = undefined;
-      this[isTouchActive] = undefined;
+    [eventAndOrCallback](eventName, detail) {
+      if (this[activeEventOrCallback] <= 0) {
+        let cbName = eventName + "Callback";
+        this[cbName] && this[cbName](detail);
+      }
+      if (this[activeEventOrCallback] >= 0)
+        this.dispatchEvent(new CustomEvent(eventName, {bubbles: true, detail}));
     }
   }
 };
