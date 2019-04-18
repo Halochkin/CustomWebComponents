cda2e7be96c8576485bbd263bc26739751e54c6b
Maks Halochkin <33881747+Halochkin@users.noreply.github.com>
2019-04-18 19:44:30 +0300

    Update touchSwipeTest.js

--- EventComposition/Gestures/Swipe/test/touch/touchSwipeTest.js
+++ EventComposition/Gestures/Swipe/test/touch/touchSwipeTest.js
@@ -1,81 +1,79 @@
-<!DOCTYPE html>
-<!--suppress ALL -->
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <style>
-        #elem {
-            position: absolute;
-            width: 450px;
-            height: 450px;
-            background-color: red;
-        }
-
-        #viewport {
-            display: inline-block;
-            width: 450px;
-            height: 450px;
-            position: relative;
-            overflow: hidden;
-            background-color: yellow;
-            margin: 10px;
-            /*margin-top: 20px;*/
-            border-radius: 10px;
-        }
-
-    </style>
-    <!--<script src="https://hammerjs.github.io/dist/hammer.js"></script>-->
-
-    <script src="../src/touch/Swipe.js"></script>
-    <script src="SwipeTest.js"></script>
-    <script src="//cdn.rawgit.com/hammerjs/touchemulator/0.0.2/touch-emulator.js"></script>
-    <script> TouchEmulator(); </script>
-</head>
-<body>
+//TEST1 of defaultPrevented
+var defaultPrevented = true;
+function testDefaultPrevented(e) {
+  if (e.trigger.defaultPrevented)
+    return true;
+  console.error(e.trigger.type + " not defaultPrevented");
+  defaultPrevented = false;
+  return false;
+}
+window.addEventListener("swipe-start", testDefaultPrevented);
+window.addEventListener("swipe-move", testDefaultPrevented);
+window.addEventListener("swipe-stop", testDefaultPrevented);
 
 
-<div swipe id="viewport" touch-action="pan-left" pointer-distance="30">
-    <div id="elem"></div>
-</div>
-<button style="float:right" onclick="setTimeout(function(){alert('boo!')}, 2000)">Alert after 2s</button>
+//TEST2
+var userSelectWorking = true;
+function userSelectShouldBe(e) {
+  let value = (e.type === "swipe-start" || e.type === "swipe-move") ? "none" : "";
+  if (document.children[0].style.userSelect === value)
+    return;
+  console.warn("body.style.userSelect not set to: " + value);
+  userSelectWorking = false;
+}
+window.addEventListener("swipe-start", userSelectShouldBe);
+window.addEventListener("swipe-move", userSelectShouldBe);
+window.addEventListener("swipe-stop", userSelectShouldBe);
+window.addEventListener("swipe-cancel", userSelectShouldBe);
 
-</body>
-<script>
 
-  function colorPicker() {
-    return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+//TEST3
+var targetHasAttribute = true;
+function testswipeAttribute(e) {
+  if (e.target.hasAttribute("swipe"))
+    return true;
+  console.error(e.target, "Wrong target, lacking attribute swipe");
+  return false;
+}
+window.addEventListener("swipe-start", testswipeAttribute);
+
+//TEST4
+var sequenceWorking = true;
+var sequences = [
+
+  ["swipe-start", "swipe-move", "swipe-move", "swipe-cancel"],
+  ["swipe-start", "swipe-cancel"],
+  ["swipe-start", "swipe-stop"]
+];
+var prevEvent = undefined;
+
+function checkSequence(e) {
+  let prevType = prevEvent? prevEvent.type : undefined;
+  for (let sequence of sequences) {
+    let prevPosition = sequence.indexOf(
+      prevType);
+    let position = sequence.lastIndexOf(e.type);
+    if (prevPosition < position)
+      return prevEvent = e; //true
   }
+  console.warn(e.type + " is following " + prevType);
+  sequenceWorking = false;
+}
 
-  let viewport = document.getElementById("viewport");
-
-  let swipeStart = 0;
-
-
-  window.addEventListener("swipe-start", e => {
-    swipeStart = e.x - e.target.offsetLeft;
-  });
-
-  window.addEventListener("swipe-stop", e => {
-
-    let swipeDist = swipeStart - e.x;
-    viewport.children[0].style.transitionDuration = "0.8s";
-    if (Math.abs(swipeDist) < 100) return;
-    viewport.children[0].style.transform = swipeDist > 0 ? "rotate(15deg)" : "rotate(-15deg)";
-    viewport.children[0].style.marginLeft = swipeDist > 0 ? "-650px" : "650px";
-    setTimeout(() => {
-      if (!viewport.children[0].parentNode)
-        return;
-      viewport.children[0].parentNode.removeChild(viewport.children[0]);
-      let el = document.createElement("div");
-      el.id = "elem";
-      el.transitionDuration = "1s";
-      el.style.backgroundColor = colorPicker();
-      viewport.appendChild(el);
-
-    }, 500);
-  });
-</script>
+window.addEventListener("swipe-start", checkSequence);
+window.addEventListener("swipe-move", checkSequence);
+window.addEventListener("swipe-stop", checkSequence);
+window.addEventListener("swipe-cancel", checkSequence);
 
 
-</html>
+//Report tests
+function reportTests() {
+  sequenceWorking ? console.log("OK sequence") : console.warn("ERROR sequence");
+  targetHasAttribute ? console.log("OK swipe") : console.warn("ERROR missing swipe attribute");
+  defaultPrevented ? console.log("OK defaultPrevented") : console.warn("ERROR defaultPrevented");
+  userSelectWorking ? console.log("OK userSelect") : console.warn("ERROR userSelect");
+}
+
+window.addEventListener("swipe-stop", reportTests);
+window.addEventListener("swipe-cancel", reportTests);
+window.addEventListener("swipe-fling", reportTests);
