00d29fed5c80965f7b47eba73d854947cf7df9c8
Maks Halochkin <33881747+Halochkin@users.noreply.github.com>
2019-04-18 19:43:38 +0300

    Update Swipe.js

--- EventComposition/Gestures/Swipe/src/touch/Swipe.js
+++ EventComposition/Gestures/Swipe/src/touch/Swipe.js
@@ -1,58 +1,48 @@
 (function () {
 
+  let supportsPassive = false;
+  try {
+    const opts = Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      }
+    });
+    window.addEventListener("test", null, opts);
+    window.removeEventListener("test", null, opts);
+  } catch (e) {
+  }
+  const thirdArg = supportsPassive ? {capture: true, passive: false} : true;
+
+
   function captureEvent(e, stopProp) {
     e.preventDefault();
     stopProp && e.stopImmediatePropagation ? e.stopImmediatePropagation() : e.stopPropagation();
   }
 
-  function filterOnAttribute(e, attributeName) {                                                 //4. FilterByAttribute
+  function filterOnAttribute(e, attributeName) {
     for (let el = e.target; el; el = el.parentNode) {
-      if (!el.hasAttribute)
-        return null;
-      if (el.hasAttribute(attributeName))
-        return el;
+      if (!el.hasAttribute) return null;
+      if (el.hasAttribute(attributeName)) return el;
     }
     return null;
   }
 
   function dispatchPriorEvent(target, composedEvent, trigger) {
-    if (!composedEvent || !target)
-    //todo remove this redundant check? should always be done at the level up?
-      return;
     composedEvent.preventDefault = function () {
       trigger.preventDefault();
-      trigger.stopImmediatePropagation
-        ? trigger.stopImmediatePropagation()
-        : trigger.stopPropagation();
+      trigger.stopImmediatePropagation ? trigger.stopImmediatePropagation() : trigger.stopPropagation();
     };
     composedEvent.trigger = trigger;
     return target.dispatchEvent(composedEvent);
   }
 
-  function findLastEventOlderThan(events, timeTest) {
-    for (let i = events.length - 1; i >= 0; i--) {
-      if (events[i].timeStamp < timeTest)
-        return events[i];
-    }
-    return null;
-  }
-
   function makeSwipeEvent(name, trigger) {
-    let details;
-    if (name !== "start") {
-      details = makeDetails(trigger, globalSequence.recorded[0])
-    }
-
-
     const composedEvent = new CustomEvent("swipe-" + name, {
       bubbles: true,
       composed: true,
-      detail: details
     });
     composedEvent.x = trigger.changedTouches ? parseInt(trigger.changedTouches[0].clientX) : trigger.x;
     composedEvent.y = trigger.changedTouches ? parseInt(trigger.changedTouches[0].clientY) : trigger.y;
-
-
     return composedEvent;
   }
 
@@ -66,7 +56,7 @@
 
 
   let globalSequence;
-  const touchdownInitialListener = e => onTouchdownInitial(e);
+  const touchInitialListener = e => onTouchInitial(e);
   const touchdownSecondaryListener = e => onTouchdownSecondary(e);
   const touchmoveListener = e => onTouchmove(e);
   const touchendListener = e => onTouchend(e);
@@ -87,12 +77,13 @@
     };
     document.children[0].style.userSelect = "none";
     document.children[0].style.touchAction = "none";
-    window.removeEventListener("touchstart", touchdownInitialListener, {capture: true, passive: false});
-    window.addEventListener("touchstart", touchdownSecondaryListener, {capture: true, passive: false});
-    window.addEventListener("touchmove", touchmoveListener, {capture: true, passive: false});
-    window.addEventListener("touchend", touchendListener, {capture: true, passive: false});
-    window.addEventListener("blur", onBlurListener, {capture: true, passive: false});
-    window.addEventListener("selectstart", onSelectstartListener, {capture: true, passive: false});
+    window.removeEventListener("touchstart", touchInitialListener, thirdArg);
+    window.removeEventListener("touchend", touchInitialListener, thirdArg);
+    window.addEventListener("touchstart", touchdownSecondaryListener, thirdArg);
+    window.addEventListener("touchmove", touchmoveListener, thirdArg);
+    window.addEventListener("touchend", touchendListener, thirdArg);
+    window.addEventListener("blur", onBlurListener, thirdArg);
+    window.addEventListener("selectstart", onSelectstartListener, thirdArg);
     return sequence;
   }
 
@@ -102,20 +93,22 @@
   }
 
   function stopSequence() {
-    document.children[0].style.userSelect = globalSequence.userSelectStart;                      //[9]a GrabTouch
+    document.children[0].style.userSelect = globalSequence.userSelectStart;
     document.children[0].style.touchAction = globalSequence.touchActionStart;
-    window.removeEventListener("touchmove", touchmoveListener, {capture: true, passive: false});
-    window.removeEventListener("touchend", touchendListener, {capture: true, passive: false});
-    window.removeEventListener("blur", onBlurListener, {capture: true, passive: false});
-    window.removeEventListener("selectstart", onSelectstartListener, {capture: true, passive: false});
-    window.removeEventListener("touchstart", touchdownSecondaryListener, {capture: true, passive: false});
-    window.addEventListener("touchdown", touchdownInitialListener, {capture: true, passive: false});
+    window.removeEventListener("touchmove", touchmoveListener, thirdArg);
+    window.removeEventListener("touchend", touchendListener, thirdArg);
+    window.removeEventListener("blur", onBlurListener, thirdArg);
+    window.removeEventListener("selectstart", onSelectstartListener, thirdArg);
+    window.removeEventListener("touchstart", touchdownSecondaryListener, thirdArg);
+    window.addEventListener("touchdown", touchInitialListener, thirdArg);
+    window.addEventListener("touchend", touchInitialListener, thirdArg);
+    return undefined;
   }
 
-  function onTouchdownInitial(trigger) {
-
-    const touches = trigger.targetTouches.length;
-    if (touches > 1)
+  function onTouchInitial(trigger) {
+    if (trigger.defaultPrevented)
+      return;
+    if (trigger.touches.length !== 1)           //support sloppy finger
       return;
     const target = filterOnAttribute(trigger, "swipe");
     if (!target)
@@ -134,13 +127,6 @@
   }
 
   function onTouchmove(trigger) {
-    if (globalSequence.cancelMouseout || mouseJailbreakOne(trigger)) {
-      const cancelEvent = makeSwipeEvent("cancel", trigger);
-      const target = globalSequence.target;
-      globalSequence = stopSequence();
-      dispatchPriorEvent(target, cancelEvent, trigger);
-      return;
-    }
     const composedEvent = makeSwipeEvent("move", trigger);
     captureEvent(trigger, false);
     globalSequence = updateSequence(globalSequence, composedEvent);
@@ -148,16 +134,13 @@
   }
 
   function onTouchend(trigger) {
+    trigger.preventDefault();
     const stopEvent = makeSwipeEvent("stop", trigger);
     if (!stopEvent) return;
     captureEvent(trigger, false);
     const target = globalSequence.target;
     globalSequence = stopSequence();
     dispatchPriorEvent(target, stopEvent, trigger);
-  }
-
-  function mouseJailbreakOne(trigger) {
-    return !(trigger.touches[0].clientY > 0 && trigger.touches[0].clientX > 0 && trigger.touches[0].clientX < window.innerWidth && trigger.touches[0].clientY < window.innerHeight);
   }
 
   function onBlur(trigger) {
@@ -169,8 +152,8 @@
 
   function onSelectstart(trigger) {
     trigger.preventDefault();
+    trigger.stopImmediatePropagation ? trigger.stopImmediatePropagation() : trigger.stopPropagation();
   }
 
-  window.addEventListener("touchstart", touchdownInitialListener, {passive: false});
-
+  document.addEventListener("touchstart", touchInitialListener, thirdArg);
 })();
