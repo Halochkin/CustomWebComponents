8cc6c4150594eae145575094465a966d5832c899
Maks Halochkin <33881747+Halochkin@users.noreply.github.com>
2019-05-10 16:50:05 +0300

    Update touch-hover.js

--- EventComposition/Events/TouchHover/src/touch-hover.js
+++ EventComposition/Events/TouchHover/src/touch-hover.js
@@ -1,98 +1,137 @@
- /**
-   * Touch-hover, mimics ':hover' which bubbles.
-   *
-   * Elements with 'touch-hover' attribute will receive a "touch-hover" event when
-   * a single touch pointer pressed to the element with 'touch-hover="click"' attribute
-   * or leaves the boundaries of an element.
-   * The "touch-hover" event has two boolean details with a text string:
-   *  detail.enter: true when the touch enters the element
-   *  detail.leave: true when the touch leaves the element
-   *
-   * If the "touch-hover" attribute has the value "click", then the touch-hover event will
-   * dispatch a "click" event on the element if the user lifts his touch finger on that element.
-   *
-   * Att!!
-   * The "touch-hover" event listens for touchmove with "passive: false" globally. This is very heavy, use with caution.
-   *
-   * Problem: TouchTarget
-   * https://stackoverflow.com/questions/3918842/how-to-find-out-the-actual-event-target-of-touchmove-javascript-event
-   */
-  (function () {
+/**
+ * Touch-hover, mimics ':hover' which bubbles.
+ *
+ * Elements with 'touch-hover' attribute will receive a "touch-hover" event when
+ * a single touch pointer pressed to the element with 'touch-hover="click"' attribute
+ * or leaves the boundaries of an element.
+ * The "touch-hover" event has two boolean details with a text string:
+ *  detail.enter: true when the touch enters the element
+ *  detail.leave: true when the touch leaves the element
+ *
+ * If the "touch-hover" attribute has the value "click", then the touch-hover event will
+ * dispatch a "click" event on the element if the user lifts his touch finger on that element.
+ *
+ * Att!!
+ * The "touch-hover" event listens for touchmove with "passive: false" globally. This is very heavy, use with caution.
+ *
+ * Problem: TouchTarget
+ * https://stackoverflow.com/questions/3918842/how-to-find-out-the-actual-event-target-of-touchmove-javascript-event
+ *
+ * Problem2: context menu on long press
+ * We do not want to make the initial touchstart event listener passive. That means that it will open the context
+ * menu if pressed for a long time and not moving. It is not critical, but it probably would have been removed if we
+ * wanted. To fix this bug, the element with the `touch-hover` attribute could be added with style `touch-action: none`
+ * for better performance too.
+ */
+(function () {
 
-    var prevTarget = undefined;
-    var relatedTarget = undefined;
-
-    function findParentWithAttribute(node, attName) {
-      for (var n = node; n; n = (n.parentNode || n.host)) {
-        if (n.hasAttribute && n.hasAttribute(attName))
-          return n;
+  var supportsPassive = false;
+  try {
+    var opts = Object.defineProperty({}, 'passive', {
+      get: function () {
+        supportsPassive = true;
       }
-      return undefined;
-    }
+    });
+    window.addEventListener("test", null, opts);
+    window.removeEventListener("test", null, opts);
+  } catch (e) {
+  }
+  var thirdArg = supportsPassive ? {passive: false, capture: true} : true;
 
-    function dispatchTouchHover(target, enter, name) {
-      if (target) {
-        setTimeout(function () {
-          var detail = {enter: enter, leave: !enter};
-          target.dispatchEvent(new CustomEvent("touch-" + name, {bubbles: true, composed: true, detail}));
-        }, 0);
-      }
-    }
+  var relatedTarget = undefined;
+  var initialUserSelect = undefined;
 
-    function getTarget(e) {
-      var pos = (e.touches && e.touches.length) ? e.touches[0] : e;
-      var target = document.elementFromPoint(pos.clientX, pos.clientY);
-      return findParentWithAttribute(target, "touch-hover");
+  function findParentWithAttribute(node, attName) {
+    for (var n = node; n; n = (n.parentNode || n.host)) {
+      if (n.hasAttribute && n.hasAttribute(attName))
+        return n;
     }
+    return undefined;
+  }
 
-    function onTouchmove(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      let touchHoverTarget = getTarget(e);
-      if (!touchHoverTarget || touchHoverTarget === relatedTarget)
-        return;
-      dispatchTouchHover(relatedTarget, false, "hover");
-      dispatchTouchHover(touchHoverTarget, true, "hover");
-      relatedTarget = touchHoverTarget;
-    }
+  function dispatchTouchHover(target, name) {
+    setTimeout(function () {
+      target.dispatchEvent(new CustomEvent("touch-" + name, {bubbles: true, composed: true}));
+    }, 0);
+  }
 
-    function init(e) {
-      e.touches.length === 1 ? start(e) : end(e);
-    }
+  function getTarget(e) {                                                     //[3a]
+    var finger = e.touches[0];
+    var target = document.elementFromPoint(finger.clientX, finger.clientY);
+    return findParentWithAttribute(target, "touch-hover");                    //[3b]
+  }
 
-    function start(e) {                                               //[2]
-      /*Check if the event starts with a target with an attribute?
-      To avoid scrolling prevention, if the event starts from another node.*/
-      if (getTarget(e)){                                               //[2a]
-        document.addEventListener("touchmove", onTouchmove, {passive: false});
-        document.children[0].style.userSelect = "none";
-      }
-    }
+  function onTouchmove(e) {
+    e.preventDefault();                                                        //[3]
+    var touchHoverTarget = getTarget(e);
+    if (touchHoverTarget === relatedTarget)
+      return;
+    if (relatedTarget)
+      dispatchTouchHover(relatedTarget, "leave");                         //[4]
+    relatedTarget = touchHoverTarget;
+    if (touchHoverTarget)
+      dispatchTouchHover(touchHoverTarget, "hover");
+  }
 
-    function end(e) {
-      document.children[0].style.userSelect = "";
-      document.removeEventListener("touchmove", onTouchmove);
-      prevTarget = undefined;
-      if (relatedTarget) {
-        dispatchTouchHover(relatedTarget, false, "cancel");
-        var clickMe = relatedTarget;
-        if (relatedTarget.getAttribute("touch-hover") === "click") {
-          setTimeout(function () {
-            clickMe.click();
-          }, 0);
-        }
-        relatedTarget = undefined;
-      }
-    }
+  function end() {
+    setBackEventListeners();
+    if (!relatedTarget)
+      return;
+    dispatchTouchHover(relatedTarget, "leave");                                 //[5a]
+    if (relatedTarget.getAttribute("touch-hover") === "click")
+      setTimeout(relatedTarget.click.bind(relatedTarget), 0);                  //[5b]
+    relatedTarget = undefined;
+  }
 
-    function cancel() {
-      /*Remove "touchmove" event listener*/
-      end();
-      if (relatedTarget)
-        dispatchTouchHover(relatedTarget, true, "cancel");
-    }
+  function cancel() {                                                          //[6]
+    setBackEventListeners();
+    if (!relatedTarget)
+      return;
+    dispatchTouchHover(relatedTarget, "leave");
+    dispatchTouchHover(relatedTarget, "cancel");
+    relatedTarget = undefined;
+  }
 
-    document.addEventListener("touchend", init);
-    document.addEventListener("touchstart", init);
+  function setBackEventListeners() {
+    document.removeEventListener("touchmove", onTouchmove, thirdArg);
+    window.removeEventListener("blur", cancel);
+    document.removeEventListener("touchend", end);
+    document.removeEventListener("touchstart", cancel);
+    document.addEventListener("touchstart", start);
+    document.addEventListener("touchend", start);                              //[1]
+
+    document.children[0].style.userSelect = initialUserSelect;                //[5]
+    initialUserSelect = undefined;
+  }
+
+  function setupActiveListeners() {
+    document.removeEventListener("touchend", start);
+    document.removeEventListener("touchstart", start);
+    document.addEventListener("touchend", end);
+    document.addEventListener("touchstart", cancel);
     window.addEventListener("blur", cancel);
-  })();
+    document.addEventListener("touchmove", onTouchmove, thirdArg);           //[2a]
+
+    initialUserSelect = document.children[0].style.userSelect;               //[2b]
+    document.children[0].style.userSelect = "none";                          //[2c]
+  }
+
+  function start(e) {                                                           //[1a]
+    if (e.touches.length !== 1)
+      return;
+    var touchHoverTarget = getTarget(e);
+    if (!touchHoverTarget)
+      return;
+    // e.preventDefault();
+    // see problem 2:
+    // the start listeners are not passive, to prevent them making the scroll behavior laggy?
+    setupActiveListeners();                                                     //[2]
+    dispatchTouchHover(touchHoverTarget, "hover");
+    relatedTarget = touchHoverTarget;
+  }
+
+  document.addEventListener("touchstart", start);        //thirdArg here too?? or tell the user to add touch-action none at the appropriate place? i think touch-action: none is best!
+  //problem 2: not making this one passive. This means that scrolling and context menu default actions can enter
+  //before we start our activities, which will block our activities.
+  document.addEventListener("touchend", start);                                 //[1]
+})();
