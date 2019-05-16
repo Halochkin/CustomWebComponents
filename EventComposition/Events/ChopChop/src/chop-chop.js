b12fa4bfa722fdd98444a183eda6e2755cc2b225
Halochkin <maksgalochkin@mail.ru>
2019-05-16 19:53:52 +0300

    chop-chop updated

--- EventComposition/Events/ChopChop/src/chop-chop.js
+++ EventComposition/Events/ChopChop/src/chop-chop.js
@@ -1,51 +1,76 @@
 (function () {
+  let beta = document.querySelector("#beta");
+  let gamma = document.querySelector("#gamma");
+  let hand2 = document.querySelector("#hand");
+  let movedBeta = document.querySelector("#moved");
 
-// Ranges in which the initial event is activated
-  const gammaDiapasone = [50, 90];  //"axe sharp" vertical angle  (right hand: / -50deg  | -90deg; left hand: \ -50deg, | -90deg;
-  const betaDiapasone = [50, -45];  //horisontal angle
-  let hand = undefined;  //in right hand gamma value will be negative, in left - positive.
+  // Ranges in which the initial event is activated
+  const gammaDiapasone = [50, 90]; //"axe sharp" vertical angle  (right hand: / -50deg  | -90deg; left hand: \ -50deg, | -90deg;
+  const betaDiapasone = [50, -45]; //horisontal angle
+  const movedAngle = 35;
+  let chops = 2; // number of chops to activate gesture
+  // let hand = undefined; //in right hand gamma value will be negative, in left - positive.
+  let initialAngle = undefined;
 
-  let firstChop = false;
+  // let deviceintheborder = undefined;
+
+
+  let backward = false;
+
+  let counter = 0;
 
 
   function checkInitalAngle(e, hand) {
-    if (hand === "right") {
-      if (e.gamma < -gammaDiapasone[0] && e.gamma > -gammaDiapasone[1] && e.beta < betaDiapasone[0]) {
-        firstChop = true;
+    // if (hand === "right") {
+
+    hand2.innerText += "+";
+
+    if (e.gamma < -gammaDiapasone[0] && e.gamma > -gammaDiapasone[1] && e.beta < betaDiapasone[0]) {
+
+      if (!initialAngle)
+        initialAngle = e.beta;
+
+      if (!backward && Math.abs(initialAngle - e.beta) > movedAngle) {
+        initialAngle = e.beta;
+        backward = true;
+        document.body.style.backgroundColor = "lightgreen";
+        counter++;
+      }
+
+      if (backward && Math.abs(initialAngle - e.beta) > movedAngle) {
+        document.body.style.backgroundColor = "yellow";
+        initialAngle = undefined;
+        backward = false;
+        counter++;
+      }
+      if (counter === 0) {
+        counter = 0;
         return true;
       }
+    } else {
+      initialAngle = undefined;
     }
 
     return false;
   }
 
-  function checkMovingAngle(e) {
-    // while(Math.abs(e.gamma))
-  }
-
-
   function handleOrientation(e) {
-    if (!hand)
-      hand = e.gamma < 0 ? "right" : "left";
 
-    if (!firstChop)
-      if (!checkInitalAngle(e, hand))
-        return;
-    // if (firstChop)
-    //   checkInitalAngle(e,);
 
-    window.dispatchEvent(new CustomEvent("chop-chop", {bubbles: true, composed: true}));
+    if (!hand) hand = e.gamma < 0 ? "right" : "left";
 
+    // if (!firstChop)
+    if (checkInitalAngle(e, hand))
+      window.dispatchEvent(
+        new CustomEvent("chop-chop", {bubbles: true, composed: true})
+      );
   }
 
 
-  if ('ondeviceorientationabsolute' in window) {
+  if ("ondeviceorientationabsolute" in window) {
     // Chrome 50+ specific
-    window.addEventListener('deviceorientationabsolute', handleOrientation)
-  }
-  else if ('ondeviceorientation' in window) {
-    window.addEventListener('deviceorientation', handleOrientation);
+    window.addEventListener("deviceorientationabsolute", handleOrientation);
+  } else if ("ondeviceorientation" in window) {
+    window.addEventListener("deviceorientation", handleOrientation);
   }
 })();
-
-
